54a14f96681ec2789fe99f1fedef723f1056cf1f
devandie <louis.devandiere@fastconnect.fr>
2015-08-03 17:15:18 +0200

    UPDATE refactor utils.py to add helper function to request Azure, and implement it in instance.py

--- plugin/instance.py
+++ plugin/instance.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+#Local import
 from plugin import utils
 from plugin import constants
 from plugin import connection
+#Azure imports
 from azure.servicemanagement import (ServiceManagementService,
                                      LinuxConfigurationSet,
-                                     OSVirtualHardDisk
+                                     OSVirtualHardDisk,
                                      )
+from azure.storage import BlobService
 from azure import (WindowsAzureConflictError,
                    WindowsAzureError
                    )
+#Cloudify imports
 from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError
 from cloudify.decorators import operation
@@ -27,45 +31,41 @@ def start(**_):
 
     azure_client = connection.AzureConnectionClient().client()
 
-    linux_config = LinuxConfigurationSet(
-        ctx.node.properties['name'],
+    linux_config = LinuxConfigurationSet(ctx.node.properties['name'],
         'administrateur',
         'Azerty@01',
-        disable_ssh_password_authentication=False
-    )
+        disable_ssh_password_authentication=False)
+
+    blob_service = connection.AzureConnectionClient().storageClient()
+
+    blob_url = blob_service.make_blob_url(ctx.node.properties['storage_container'],
+                    '{0}.vhd'.format(ctx.node.properties['name']),
+                    ctx.node.properties['storage_account'])
 
     os_hd = OSVirtualHardDisk(ctx.node.properties['image_id'],
-                              ctx.node.properties['storage_account_url'])
+                              blob_url)
 
-    try:
-        azure_client.create_virtual_machine_deployment(
-            service_name=ctx.node.properties['cloud_service'],
-            deployment_name=ctx.node.properties['name'],
-            deployment_slot='production',
-            label=ctx.node.properties['name'],
-            role_name=ctx.node.properties['name'],
-            system_config=linux_config,
-            os_virtual_hard_disk=os_hd,
-            role_size='Small'
-            )
-    except WindowsAzureConflictError as e:
-        raise NonRecoverableError.message('ERROR: Conflict error,\
-                                           already used name'
-                                         )
-    except WindowsAzureError as e:
-        raise NonRecoverableError.message('ERROR: {0}'.format(e))
-
-    status = azure_client.get_deployment_by_name(
-                            ctx.node.properties['cloud_service'],
-                            ctx.node.properties['name']
-                            ).status
+    ctx.logger.info('Trying to deploy {0} {1} {2}'
+                    .format(ctx.node.properties['cloud_service'],
+                        ctx.node.properties['name'],
+                        blob_url))
+    utils.azure_request(ctx,azure_client, 'create_virtual_machine_deployment',
+                        service_name=ctx.node.properties['cloud_service'],
+                        deployment_name=ctx.node.properties['name'], 
+                        deployment_slot='production',
+                        label=ctx.node.properties['name'],
+                        role_name=ctx.node.properties['name'],
+                        system_config=linux_config,
+                        os_virtual_hard_disk=os_hd,
+                        role_size='Small'
+                        )
+   
+    status = azure_client.get_deployment_by_name(ctx.node.properties['cloud_service'],
+                            ctx.node.properties['name']).status
 
     ctx.logger.info('{0} instance is {1}'
-                    .format(
-                        ctx.node.properties['name'],
-                        status
-                    )
-    )
+                    .format(ctx.node.properties['name'],
+                        status))
     return status
 
 
@@ -74,21 +74,10 @@ def stop(**_):
 
     azure_client = connection.AzureConnectionClient().client()
 
-    result = azure_client.delete_deployment(
-        service_name=ctx.node.properties['cloud_service'],
-        deployment_name=ctx.node.properties['name']
-        )
+    utils.azure_request(ctx, azure_client, 'delete_deployment',
+                        ctx.node.properties['cloud_service'],
+                        ctx.node.properties['name'], 
+                        True
+                        )
 
-    status = azure_client.get_operation_status(result.request_id).status
-    while status != 'Succeeded':
-        status = azure_client.get_operation_status(result.request_id).status
-
-    #TODO delete os_disk
-
-    ctx.logger.info('{0} instance is {1}'
-                    .format(
-                        ctx.node.properties['name'],
-                        status
-                    )
-    )
-    return status
+    return constants.REQUEST_SUCCEEDED
--- plugin/utils.py
+++ plugin/utils.py
@@ -1,4 +1,4 @@
-########
+﻿########
 # Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,6 +20,7 @@ import os
 from plugin import constants
 from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError
+from azure import WindowsAzureError
 
 
 def validate_node_property(key, ctx_node_properties):
@@ -45,46 +46,6 @@ def log_available_resources(list_of_resources):
     ctx.logger.debug(message)
 
 
-def get_external_resource_id_or_raise(operation, ctx_instance):
-    """Checks if the EXTERNAL_RESOURCE_ID runtime_property is set,
-    and returns it.
-
-    :param operation: A string representing what is happening.
-    :param ctx_instance: The CTX Node-Instance Context.
-    :param ctx:  The Cloudify ctx context.
-    :returns The EXTERNAL_RESOURCE_ID runtime_property for a CTX Instance.
-    :raises NonRecoverableError: If EXTERNAL_RESOURCE_ID has not been set.
-    """
-
-    ctx.logger.debug(
-        'Checking if {0} in instance runtime_properties, for {0} operation.'
-        .format(constants.EXTERNAL_RESOURCE_ID, operation))
-
-    if constants.EXTERNAL_RESOURCE_ID not in ctx_instance.runtime_properties:
-        raise NonRecoverableError(
-            'Cannot {0} because {1} is not assigned.'
-            .format(operation, constants.EXTERNAL_RESOURCE_ID))
-
-    return ctx_instance.runtime_properties[constants.EXTERNAL_RESOURCE_ID]
-
-
-def set_external_resource_id(value, ctx_instance, external=True):
-    """Sets the EXTERNAL_RESOURCE_ID runtime_property for a Node-Instance.
-
-    :param value: the desired EXTERNAL_RESOURCE_ID runtime_property
-    :param ctx:  The Cloudify ctx context.
-    :param external:  Boolean representing if it is external resource or not.
-    """
-
-    if not external:
-        resource_type = 'Cloudify'
-    else:
-        resource_type = 'external'
-
-    ctx.logger.info('Using {0} resource: {1}'.format(resource_type, value))
-    ctx_instance.runtime_properties[constants.EXTERNAL_RESOURCE_ID] = value
-
-
 def unassign_runtime_property_from_resource(property_name, ctx_instance):
     """Pops a runtime_property and reports to debug.
 
@@ -96,86 +57,6 @@ def unassign_runtime_property_from_resource(property_name, ctx_instance):
     value = ctx_instance.runtime_properties.pop(property_name)
     ctx.logger.debug(
         'Unassigned {0} runtime property: {1}'.format(property_name, value))
-
-
-def use_external_resource(ctx_node_properties):
-    """Checks if use_external_resource node property is true,
-    logs the ID and answer to the debug log,
-    and returns boolean False (if not external) or True.
-
-    :param node_properties: The ctx node properties for a node.
-    :param ctx:  The Cloudify ctx context.
-    :returns boolean: False if not external.
-    """
-
-    if not ctx_node_properties['use_external_resource']:
-        ctx.logger.debug(
-            'Using Cloudify resource_id: {0}.'
-            .format(ctx_node_properties['resource_id']))
-        return False
-    else:
-        ctx.logger.debug(
-            'Using external resource_id: {0}.'
-            .format(ctx_node_properties['resource_id']))
-        return True
-
-
-def get_target_external_resource_ids(relationship_type, ctx_instance):
-    """Gets a list of target node ids connected via a relationship to a node.
-
-    :param relationship_type: A string representing the type of relationship.
-    :param ctx:  The Cloudify ctx context.
-    :returns a list of security group ids.
-    """
-
-    ids = []
-
-    if not getattr(ctx_instance, 'relationships', []):
-        ctx.logger.info('Skipping attaching relationships, '
-                        'because none are attached to this node.')
-        return ids
-
-    for r in ctx_instance.relationships:
-        if relationship_type in r.type:
-            ids.append(
-                r.target.instance.runtime_properties[
-                    constants.EXTERNAL_RESOURCE_ID])
-
-    return ids
-
-
-def get_resource_id():
-    """Returns the resource id, if the user doesn't provide one,
-    this will create one for them.
-
-    :param node_properties: The node properties dictionary.
-    :return resource_id: A string.
-    """
-
-    if ctx.node.properties['resource_id']:
-        return ctx.node.properties['resource_id']
-    elif 'private_key_path' in ctx.node.properties:
-        directory_path, filename = \
-            os.path.split(ctx.node.properties['private_key_path'])
-        resource_id, filetype = filename.split('.')
-        return resource_id
-
-    return '{0}-{1}'.format(ctx.deployment.id, ctx.instance.id)
-
-
-def get_provider_variables():
-
-    provider_config = ctx.provider_context.get('resources', {})
-
-    agents_keypair = provider_config.get('agents_keypair', {})
-    agents_security_group = provider_config.get('agents_security_group', {})
-
-    provider_context = {
-        "agents_keypair": agents_keypair.get('id'),
-        "agents_security_group": agents_security_group.get('id')
-    }
-
-    return provider_context
 
 
 def get_instance_or_source_node_properties():
@@ -192,3 +73,35 @@ def get_instance_or_source_node_properties():
                 .format(
                     constants.RELATIONSHIP_INSTANCE,
                     constants.NODE_INSTANCE))
+
+
+def azure_request(ctx, service_management, request, *args,**kwargs):
+    """ A helper to send request to Azure. The operation status
+    is check to monitor the request. Failures are managed too.
+
+    :param ctx: The Cloudify context to log information.
+    :param service_management: The service management object to contact Azure.
+    :param request: The request to send. The request's result has to be an
+    operation.
+    """
+    ctx.logger.info('Trying to perform {0}...'.format(request))
+    
+    try:
+        resp = getattr(service_management, request)(*args, **kwargs)
+
+        ctx.logger.info('Operation in progress...')
+
+        operation = service_management.get_operation_status(resp.request_id)
+        while operation.status == constants.REQUEST_IN_PROGRESS:
+            operation = service_management.\
+                get_operation_status(resp.request_id)
+
+        if operation.status == constants.REQUEST_FAILED:
+            ctx.logger.info(
+                'Operation failed: {0}...'.format(operation.error.message)
+                )
+        else:
+            ctx.logger.info('Operation Succeeded.')
+    except WindowsAzureError as e:
+        ctx.logger.info('WindowsAzureError: {0}'.format(e.message))
+        raise NonRecoverableError()
