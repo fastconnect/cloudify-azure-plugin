4a5697bf3f6a80f3fefab60fa8bd46a28c38c671
chardonj <jonathan.chardon@fastconnect.fr>
2015-08-17 17:33:34 +0200

    ADD(35-36) test conflict still bugy :-(

--- plugin/instance.py
+++ plugin/instance.py
@@ -166,31 +166,64 @@ def get_vm_provisioning_state(**_):
     status_vm = jsonGet['properties']['provisioningState']
     return status_vm
 
-
 def get_nic_virtual_machine_id(**_):
     utils.validate_node_property('subscription_id', ctx.node.properties)
     utils.validate_node_property('resource_group_name', ctx.node.properties)
+    utils.validate_node_property('network_interface_name', ctx.node.properties)
 
     subscription_id = ctx.node.properties['subscription_id']
     api_version = constants.AZURE_API_VERSION_06
     resource_group_name = ctx.node.properties['resource_group_name']
-
-    response = connection.AzureConnectionClient().azure_get(
-            ctx, 
-            ("subscriptions/{}/resourcegroups/{}/"+
-            "providers/microsoft.network/networkInterfaces"+
-            "/?api-version={}").format(
-                subscription_id, 
-                resource_group_name, 
-                api_version
-            )
-        )
-    jsonGet = response.json()
-    ctx.logger.debug(jsonGet)
+    network_interface_name = ctx.node.properties['network_interface_name']
     try:
+        response = connection.AzureConnectionClient().azure_get(
+                ctx, 
+                ("subscriptions/{}/resourcegroups/{}/"+
+                "providers/microsoft.network/networkInterfaces/{}"+
+                "/?api-version={}").format(
+                    subscription_id, 
+                    resource_group_name,
+                    network_interface_name, 
+                    api_version
+                )
+            )
+        jsonGet = response.json()
+        ctx.logger.debug(jsonGet)
+
         vm_id = jsonGet['properties']['virtualMachine']['id']
     except KeyError :
-        vm_id =  None
-        
+        return None
+     
     return vm_id
 
+
+
+def get_disks_statuses(**_):
+    utils.validate_node_property('subscription_id', ctx.node.properties)
+    utils.validate_node_property('resource_group_name', ctx.node.properties)
+    utils.validate_node_property('network_interface_name', ctx.node.properties)
+
+    subscription_id = ctx.node.properties['subscription_id']
+    api_version = constants.AZURE_API_VERSION_06
+    resource_group_name = ctx.node.properties['resource_group_name']
+    network_interface_name = ctx.node.properties['network_interface_name']
+    try:
+        response = connection.AzureConnectionClient().azure_get(
+                ctx, 
+                ("subscriptions/{}/resourcegroups/{}/"+
+                "providers/microsoft.network/networkInterfaces/{}"+
+                "/InstanceView?api-version={}").format(
+                    subscription_id, 
+                    resource_group_name,
+                    network_interface_name, 
+                    api_version
+                )
+            )
+        jsonGet = response.json()
+        ctx.logger.debug(jsonGet)
+
+        disks_statuses = jsonGet['disks']['statuses']['code']
+    except KeyError :
+        return None
+     
+    return disks_statuses
