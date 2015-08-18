c33257d1027891247f03f1a538fcc509e7791750
chardonj <jonathan.chardon@fastconnect.fr>
2015-08-18 15:03:22 +0200

    move ctx_mock function into test_utils file

--- plugin/tests/test_instance.py
+++ plugin/tests/test_instance.py
@@ -15,47 +15,16 @@ TIME_DELAY = 30
 
 class TestInstance(testtools.TestCase):
 
-    def mock_ctx(self, test_name):
-        """ Creates a mock context for the instance
-            tests
-        """
-
-        test_properties = {
-            'subscription_id': test_utils.SUBSCRIPTION_ID,
-            'username': test_utils.AZURE_USERNAME, 
-            'password': test_utils.AZURE_PASSWORD,
-            'location': 'westeurope',
-            'publisherName': 'Canonical',
-            'offer': 'UbuntuServer',
-            'sku': '12.04.5-LTS',
-            'version': 'latest',
-            'flavor_id': 'Standard_A1',
-            'compute_name': test_name,
-            'compute_user': test_utils.COMPUTE_USER,
-            'compute_password': test_utils.COMPUTE_PASSWORD,
-            'resources_prefix': 'boulay',
-            'network_interface_name': 'cloudifynic',
-            'storage_account': 'cloudifystorageaccount',
-            'create_option':'FromImage',
-            'resource_group_name': 'cloudifygroup',
-            'management_network_name': 'cloudifynetwork',
-            'management_subnet_name': 'cloudifysubnet'
-        }
-
-        return MockCloudifyContext(node_id='test',
-                                   properties=test_properties)
-
-
     def setUp(self):
         super(TestInstance, self).setUp()
 
 
     def tearDown(self):
         super(TestInstance, self).tearDown()
-        time.sleep(30)
+        time.sleep(TIME_DELAY)
 
     def test_create(self):
-        ctx = self.mock_ctx('testcreate')
+        ctx = test_utils.mock_ctx('testcreate')
         ctx.logger.info("BEGIN create VM test")
 
         ctx.logger.info("create VM")    
@@ -67,7 +36,7 @@ class TestInstance(testtools.TestCase):
         status_vm = constants.CREATING
         while status_vm == constants.CREATING :
             status_vm = instance.get_vm_provisioning_state(ctx=ctx)
-            time.sleep(20)    
+            time.sleep(TIME_DELAY)    
         
         ctx.logger.info("check VM creation success")
         current_ctx.set(ctx=ctx)
@@ -82,12 +51,12 @@ class TestInstance(testtools.TestCase):
         while nic_machine_id is not None :
             current_ctx.set(ctx=ctx)
             nic_machine_id = instance.get_nic_virtual_machine_id(ctx=ctx)
-            time.sleep(20)
+            time.sleep(TIME_DELAY)
         ctx.logger.info("END create VM test")
 
     def test_delete(self):
     
-        ctx = self.mock_ctx('testdelete')
+        ctx = test_utils.mock_ctx('testdelete')
         ctx.logger.info("BEGIN delete VM test")
     
         ctx.logger.info("create VM")    
@@ -99,7 +68,7 @@ class TestInstance(testtools.TestCase):
         while status_vm == constants.CREATING :
             current_ctx.set(ctx=ctx)
             status_vm = instance.get_vm_provisioning_state(ctx=ctx)
-            time.sleep(20)
+            time.sleep(TIME_DELAY)
         
         ctx.logger.info("check VM creation success")
         current_ctx.set(ctx=ctx)
@@ -117,11 +86,11 @@ class TestInstance(testtools.TestCase):
                                     ctx=ctx
                                 )
 
-            time.sleep(20)
+            time.sleep(TIME_DELAY)
         ctx.logger.info("END delete VM test")
 
     def test_conflict(self):
-        ctx = self.mock_ctx('testconflict')
+        ctx = test_utils.mock_ctx('testconflict')
 
         ctx.logger.info("BEGIN conflict VM test")
 
@@ -136,7 +105,7 @@ class TestInstance(testtools.TestCase):
             status_vm = instance.get_vm_provisioning_state(
                             ctx=ctx
                         )
-            time.sleep(20)
+            time.sleep(TIME_DELAY)
 
         ctx.logger.info("VM creation conflict")
         current_ctx.set(ctx=ctx)
@@ -156,7 +125,7 @@ class TestInstance(testtools.TestCase):
             nic_machine_id = instance.get_nic_virtual_machine_id(
                                 ctx=ctx
                             )
-            time.sleep(20)
+            time.sleep(TIME_DELAY)
         
         ctx.logger.info("check vm provisionning state in a deleted machine")
         current_ctx.set(ctx=ctx)
@@ -171,5 +140,5 @@ class TestInstance(testtools.TestCase):
         self.assertEqual(204, instance.delete(ctx=ctx))
        
     def test_stop(self):
-        ctx = self.mock_ctx('teststop')
+        ctx = test_utils.mock_ctx('teststop')
         current_ctx.set(ctx=ctx)
