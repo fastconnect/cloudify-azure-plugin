c143eb140a4fe8596919e714100cb01ca2228859
chardonj <jonathan.chardon@fastconnect.fr>
2015-08-21 12:12:37 +0200

    UNFINISHED (42-47) ressource_group + test

---
+++ plugin/ressource_group.py
@@ -0,0 +1,76 @@
+import connection
+import constants
+import utils
+
+from cloudify import ctx
+from cloudify.decorators import operation
+
+def get_ressource_group_provisioning_state(**_):
+    utils.validate_node_property('subscription_id', ctx.node.properties)
+    utils.validate_node_property('resource_group_name', ctx.node.properties)
+
+    subscription_id = ctx.node.properties['subscription_id']
+    api_version = str(constants.AZURE_API_VERSION_04_PREVIEW)
+    resource_group_name = ctx.node.properties['resource_group_name']
+
+    response = connection.AzureConnectionClient().azure_get(
+            ctx, 
+            ("subscriptions/{}/resourcegroups/"+
+            "{}?api-version={}").format(
+                subscription_id, 
+                resource_group_name, 
+                api_version
+            )
+        )
+    jsonGet = response.json()
+    status_ressource_group = jsonGet['properties']['provisioningState']
+    return status_ressource_group
+
+
+def delete(**_):
+    utils.validate_node_property('subscription_id', ctx.node.properties)
+    utils.validate_node_property('resource_group_name', ctx.node.properties)
+
+    subscription_id = ctx.node.properties['subscription_id']
+    api_version = constants.AZURE_API_VERSION_04_PREVIEW
+    resource_group_name = ctx.node.properties['resource_group_name']
+
+    cntn = connection.AzureConnectionClient()
+    response = cntn.azure_put(ctx, 
+                   ("subscriptions/{}/resourcegroups/{}" +
+                    "?api-version={}").format(
+                                            subscription_id, 
+                                            resource_group_name, 
+                                            api_version
+                                            )
+                    )
+    return response.status_code
+
+
+def create(**_):
+    utils.validate_node_property('subscription_id', ctx.node.properties)
+    utils.validate_node_property('resource_group_name', ctx.node.properties)
+    utils.validate_node_property('location', ctx.node.properties)
+
+    subscription_id = ctx.node.properties['subscription_id']
+    api_version = constants.AZURE_API_VERSION_04_PREVIEW
+    resource_group_name = ctx.node.properties['resource_group_name']
+    location = ctx.node.properties['location']
+
+    json ={
+            "location": str(location)
+        }
+
+    ctx.logger.info('Beginning ressource_group creation')
+    cntn = connection.AzureConnectionClient()
+
+    response = cntn.azure_put(ctx, 
+                   ("subscriptions/{}/resourcegroups/{}" +
+                    "?api-version={}").format(
+                                            subscription_id, 
+                                            resource_group_name, 
+                                            api_version
+                                            ),
+                    json=json
+                    )
+    return response.status_code
---
+++ plugin/tests/test_ressource_group.py
@@ -0,0 +1,79 @@
+import testtools
+import time
+import test_utils
+
+from plugin import utils
+from plugin import constants
+from plugin import connection
+from plugin import ressource_group
+
+from cloudify.state import current_ctx
+from cloudify.mocks import MockCloudifyContext
+
+TIME_DELAY = 20
+
+class TestRessourceGroup(testtools.TestCase):
+
+    def mock_ctx(self, test_name):
+        """ Creates a mock context for the instance
+            tests
+        """
+
+        test_properties = {
+            'subscription_id': test_utils.SUBSCRIPTION_ID,
+            'username': test_utils.AZURE_USERNAME, 
+            'password': test_utils.AZURE_PASSWORD,
+            'location': 'westeurope',
+            'resource_group_name': test_name
+        }
+
+        return MockCloudifyContext(node_id = 'test',
+                                   properties = test_properties)
+
+    def setUp(self):
+        super(TestRessourceGroup, self).setUp()
+
+
+    def tearDown(self):
+        super(TestRessourceGroup, self).tearDown()
+        time.sleep(TIME_DELAY)
+
+    def test_create_ressource_group(self):
+        ctx = self.mock_ctx('testcreategroup')
+        current_ctx.set(ctx=ctx)
+        ctx.logger.info("BEGIN create ressource_group test")
+
+        ctx.logger.info("create ressource_group")
+        status_code = ressource_group.create(ctx=ctx)
+        ctx.logger.debug("status_code = " + str(status_code) )
+        self.assertTrue(bool((status_code == 200) or (status_code == 201)))
+
+        status_ressource_group = constants.CREATING
+        while status_ressource_group == constants.CREATING :
+            current_ctx.set(ctx=ctx)
+            ctx.logger.debug(status_ressource_group)
+            status_ressource_group = ressource_group.get_ressource_group_provisioning_state(ctx=ctx)
+            time.sleep(TIME_DELAY)
+        
+        ctx.logger.debug(status_ressource_group)
+        ctx.logger.info("check ressource_group creation success")
+        self.assertEqual( constants.SUCCEEDED, status_ressource_group)
+
+        ctx.logger.info("delete ressource_group")
+        self.assertEqual(202, ressource_group.delete(ctx=ctx))
+
+        try:
+            while status_ressource_group == constants.DELETING :
+                current_ctx.set(ctx=ctx)
+                ctx.logger.debug(status_ip)           
+                status_ressource_group = ressource_group.get_ressource_group_provisioning_state(ctx=ctx)
+                time.sleep(TIME_DELAY)
+        except utils.WindowsAzureError:
+            pass
+
+        ctx.logger.info("END create ressource_group  test")
+
+
+    #def test_delete_ressource_group(self):
+
+    #def test_conflict_ressource_group(self):
