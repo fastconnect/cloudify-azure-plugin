174fe25e93978e166eeccb56909710c9bb59da14
Victor O <victor.oudin@fastconnect.fr>
2015-09-01 11:12:11 +0200

    UPDATE test_storage; try import module except local import

--- plugin/tests/test_storage.py
+++ plugin/tests/test_storage.py
@@ -2,9 +2,14 @@ import testtools
 import time
 import test_utils
 
-from plugin import utils
-from plugin import constants
-from plugin import storage
+try:
+    from plugin import utils
+    from plugin import constants
+    from plugin import storage
+except:
+    import utils
+    import constants
+    import storage
 
 from cloudify.state import current_ctx
 from cloudify.mocks import MockCloudifyContext
