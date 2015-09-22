0afdd43e3e9791ba5fbe2c4b33f9762baee38d8f
Victor O <victor.oudin@fastconnect.fr>
2015-09-22 21:13:07 +0200

    UPDATE subnet; add get_id function (CFAZURE-110)

--- plugin/subnet.py
+++ plugin/subnet.py
@@ -8,12 +8,13 @@ from plugin import (utils,
 from cloudify import ctx
 from cloudify.decorators import operation
 
+
 @operation
 def create(**_):
     utils.validate_node_property(constants.SUBNET_KEY, ctx.node.properties)
-    utils.validate_node_property(constants.SUBNET_ADDRESS_KEY, 
+    utils.validate_node_property(constants.SUBNET_ADDRESS_KEY,
                                  ctx.node.properties)
-    utils.validate_node_property(constants.VIRTUAL_NETWORK_KEY, 
+    utils.validate_node_property(constants.VIRTUAL_NETWORK_KEY,
                                  ctx.instance.runtime_properties)
 
     azure_config = utils.get_azure_config(ctx)
@@ -21,41 +22,41 @@ def create(**_):
     subscription_id = azure_config[constants.SUBSCRIPTION_KEY]
     resource_group_name = azure_config[constants.RESOURCE_GROUP_KEY]
     virtual_network_name = ctx.instance.runtime_properties[
-                                    constants.VIRTUAL_NETWORK_KEY]
+        constants.VIRTUAL_NETWORK_KEY]
     subnet_name = ctx.node.properties[constants.SUBNET_KEY]
     subnet_address_prefix = ctx.node.properties[constants.SUBNET_ADDRESS_KEY]
     api_version = constants.AZURE_API_VERSION_05_preview
 
-    json ={
-       "properties":{
-          "addressPrefix": str(subnet_address_prefix)
-       }
+    json = {
+        "properties": {
+            "addressPrefix": str(subnet_address_prefix)
+        }
     }
 
     ctx.logger.info('Creating Subnet')
     connect = connection.AzureConnectionClient()
 
     response = connect.azure_put(ctx,
-        ("subscriptions/{}/resourceGroups/{}/" +
-            "providers/microsoft.network" +
-            "/virtualNetworks/{}" +
-            "/subnets/{}" +
-            "?api-version={}").format(
-            subscription_id,
-            resource_group_name,
-            virtual_network_name,
-            subnet_name,
-            api_version
-        ),
-        json=json
-    )
+                                 ("subscriptions/{}/resourceGroups/{}/" +
+                                  "providers/microsoft.network" +
+                                  "/virtualNetworks/{}" +
+                                  "/subnets/{}" +
+                                  "?api-version={}").format(
+                                     subscription_id,
+                                     resource_group_name,
+                                     virtual_network_name,
+                                     subnet_name,
+                                     api_version
+                                 ),
+                                 json=json
+                                 )
     return response.status_code
 
 
 @operation
 def delete(**_):
     utils.validate_node_property(constants.SUBNET_KEY, ctx.node.properties)
-    utils.validate_node_property(constants.VIRTUAL_NETWORK_KEY, 
+    utils.validate_node_property(constants.VIRTUAL_NETWORK_KEY,
                                  ctx.instance.runtime_properties)
 
     azure_config = utils.get_azure_config(ctx)
@@ -63,7 +64,7 @@ def delete(**_):
     subscription_id = azure_config[constants.SUBSCRIPTION_KEY]
     resource_group_name = azure_config[constants.RESOURCE_GROUP_KEY]
     virtual_network_name = ctx.instance.runtime_properties[
-                                            constants.VIRTUAL_NETWORK_KEY]
+        constants.VIRTUAL_NETWORK_KEY]
     subnet_name = ctx.node.properties[constants.SUBNET_KEY]
     api_version = constants.AZURE_API_VERSION_05_preview
 
@@ -71,19 +72,20 @@ def delete(**_):
     connect = connection.AzureConnectionClient()
 
     response = connect.azure_delete(ctx,
-        ("subscriptions/{}/resourceGroups/{}/" +
-            "providers/microsoft.network" +
-            "/virtualNetworks/{}" +
-            "/subnets/{}" +
-            "?api-version={}").format(
-            subscription_id,
-            resource_group_name,
-            virtual_network_name,
-            subnet_name,
-            api_version
-        )
-    )
+                                    ("subscriptions/{}/resourceGroups/{}/" +
+                                     "providers/microsoft.network" +
+                                     "/virtualNetworks/{}" +
+                                     "/subnets/{}" +
+                                     "?api-version={}").format(
+                                        subscription_id,
+                                        resource_group_name,
+                                        virtual_network_name,
+                                        subnet_name,
+                                        api_version
+                                    )
+                                    )
     return response.status_code
+
 
 @operation
 def preconfigure_subnet_connected_to_network(**_):
@@ -93,7 +95,7 @@ def preconfigure_subnet_connected_to_network(**_):
 
 
 def get_provisioning_state(**_):
-    utils.validate_node_property(constants.VIRTUAL_NETWORK_KEY, 
+    utils.validate_node_property(constants.VIRTUAL_NETWORK_KEY,
                                  ctx.instance.runtime_properties)
     utils.validate_node_property(constants.SUBNET_KEY, ctx.node.properties)
 
@@ -102,18 +104,54 @@ def get_provisioning_state(**_):
     subscription_id = azure_config[constants.SUBSCRIPTION_KEY]
     resource_group_name = azure_config[constants.RESOURCE_GROUP_KEY]
     virtual_network_name = ctx.instance.runtime_properties[
-                                            constants.VIRTUAL_NETWORK_KEY]
+        constants.VIRTUAL_NETWORK_KEY]
     subnet_name = ctx.node.properties[constants.SUBNET_KEY]
     api_version = constants.AZURE_API_VERSION_05_preview
 
     connect = connection.AzureConnectionClient()
 
     response = connect.azure_get(ctx,
-        ("subscriptions/{}/resourceGroups/{}/" +
-            "providers/microsoft.network" +
-            "/virtualNetworks/{}" +
-            "/subnets/{}" +
-            "?api-version={}").format(
+                                 ("subscriptions/{}/resourceGroups/{}/" +
+                                  "providers/microsoft.network" +
+                                  "/virtualNetworks/{}" +
+                                  "/subnets/{}" +
+                                  "?api-version={}").format(
+                                     subscription_id,
+                                     resource_group_name,
+                                     virtual_network_name,
+                                     subnet_name,
+                                     api_version
+                                 )
+                                 )
+
+    jsonGet = response.json()
+    status_storage = jsonGet['properties']['provisioningState']
+
+    return status_storage
+
+
+def get_id(ctx):
+    utils.validate_node_property(constants.VIRTUAL_NETWORK_KEY,
+                                 ctx.instance.runtime_properties)
+    utils.validate_node_property(constants.SUBNET_KEY,
+                                 ctx.instance.runtime_properties)
+    azure_config = utils.get_azure_config(ctx)
+
+    subscription_id = azure_config[constants.SUBSCRIPTION_KEY]
+    resource_group_name = azure_config[constants.RESOURCE_GROUP_KEY]
+    api_version = constants.AZURE_API_VERSION_05_preview
+    virtual_network_name = ctx.instance.runtime_properties[
+        constants.VIRTUAL_NETWORK_KEY]
+    subnet_name = ctx.instance.runtime_properties[constants.SUBNET_KEY]
+
+    response = connection.AzureConnectionClient().azure_get(
+        ctx,
+        ('subscriptions/{}' +
+         '/resourceGroups/{}' +
+         '/providers/microsoft.network' +
+         '/virtualNetworks/{}' +
+         '/subnets/{}' +
+         '?api-version={}').format(
             subscription_id,
             resource_group_name,
             virtual_network_name,
@@ -121,8 +159,4 @@ def get_provisioning_state(**_):
             api_version
         )
     )
-
-    jsonGet = response.json()
-    status_storage = jsonGet['properties']['provisioningState']
-
-    return status_storage
+    return response.json()['id']
